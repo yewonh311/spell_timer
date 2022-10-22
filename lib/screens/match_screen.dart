a1d98ce8395fe51361123f05d1b2384ce881617b
Yewon Hong <88092102+yewonhong@users.noreply.github.com>
2022-10-22 14:31:30 -0700

    Update match_screen.dart

--- lib/screens/match_screen.dart
+++ lib/screens/match_screen.dart
@@ -1,29 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:spell_checker/widgets/pop_up.dart';
 import '../models/active_game_by_summoner.dart';
-import 'search_screen.dart';
 import 'package:spell_checker/widgets/profile.dart';
 import 'package:spell_checker/widgets/game_length_timer.dart';
 import 'package:spell_checker/widgets/participants_list.dart';
-import 'package:get/get.dart';
 
 class MatchScreen extends StatefulWidget {
+  final String encryptIdFromSearch;
+  final String summonerNameFromSearch;
+  final int profileIconIdFromSearch;
+  final int summonerLevelFromSearch;
+  MatchScreen(
+      {required this.encryptIdFromSearch,
+      required this.summonerNameFromSearch,
+      required this.profileIconIdFromSearch,
+      required this.summonerLevelFromSearch});
+
   @override
-  _MatchScreenState createState() => _MatchScreenState();
+  _MatchScreenState createState() => _MatchScreenState(
+      gotEncryptId: encryptIdFromSearch,
+      gotSummonerName: summonerNameFromSearch,
+      gotProfileIconId: profileIconIdFromSearch,
+      gotSummonerLevel: summonerLevelFromSearch);
 }
 
 class _MatchScreenState extends State<MatchScreen> {
   late Future<Participants> pars;
-
-  String notFound = "Cannot found match.";
-  String matchTypeError = "Game type error";
-  String mapTypeError = "Only Summoner's Rift can be loaded";
+  final String gotEncryptId;
+  final String gotSummonerName;
+  final int gotProfileIconId;
+  final int gotSummonerLevel;
+  String notFound = "매칭중인 게임을 찾을 수 없습니다.";
   bool pressColor = false;
   bool pressText = false;
+  _MatchScreenState(
+      {required this.gotEncryptId,
+      required this.gotSummonerName,
+      required this.gotProfileIconId,
+      required this.gotSummonerLevel});
 
   @override
   void initState() {
     super.initState();
+  }
+
+  _notActive() {
+    Navigator.of(context).pop();
   }
 
   @override
@@ -34,170 +55,139 @@ class _MatchScreenState extends State<MatchScreen> {
         backgroundColor: Colors.grey.shade300,
         body: Center(
           child: FutureBuilder<Participants>(
-            future: pars = fetchParticipants(
-                encryptId: Get.parameters['param'].toString()),
+            future: pars = fetchParticipants(encryptId: gotEncryptId),
             builder: (context, snapshot) {
               switch (snapshot.connectionState) {
-                // case ConnectionState.none:
+                case ConnectionState.none:
                 case ConnectionState.waiting:
                   return CircularProgressIndicator();
                 default:
                   if (snapshot.hasData) {
-                    if (snapshot.data!.mapId == 11) {
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Stack(
-                            alignment: Alignment.topLeft,
-                            children: <Widget>[
-                              IconButton(
-                                icon: Icon(Icons.arrow_back),
-                                onPressed: () {
-                                  Get.offAll(SearchSummoner(),
-                                      transition:
-                                          Transition.leftToRightWithFade);
-                                },
-                              ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Profile(
-                                  summonerNameFromMatch:
-                                      Get.parameters['sumName'].toString(),
-                                  profileIconIdFromMatch:
-                                      Get.parameters['iconId'].toString(),
-                                  summonerLevelFromMatch:
-                                      Get.parameters['sumLevel'].toString()),
-                              Column(
-                                children: <Widget>[
-                                  _matchType(snapshot.data!.mapId,
-                                      snapshot.data!.queueType),
-                                  ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      primary:
-                                          pressColor ? Colors.blue : Colors.red,
-                                    ),
-                                    child: Text(pressText ? "전체보기" : "상대만 보기"),
-                                    onPressed: () {
-                                      setState(() {
-                                        pressColor = !pressColor;
-                                        pressText = !pressText;
-                                      });
-                                    },
-                                  ),
-                                ],
-                              ),
-                              GameLengthTimer(
-                                  gameLengthFromMatch:
-                                      snapshot.data!.gameLength.toString()),
-                            ],
-                          ),
-                          Row(children: <Widget>[
-                            Flexible(
-                              child: ListView.separated(
-                                  separatorBuilder: (context, index) =>
-                                      Divider(),
-                                  shrinkWrap: true,
-                                  scrollDirection: Axis.vertical,
-                                  itemCount: 5,
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    if (pressColor == false) {
-                                      return Row(
-                                        children: <Widget>[
-                                          ParticipantsList(
-                                              partsNamesFromMatch:
-                                                  snapshot.data?.partsNames,
-                                              spell1IdFromMatch:
-                                                  snapshot.data?.spell1Id,
-                                              spell1CoolDownFromMatch:
-                                                  snapshot.data?.spell1CoolDown,
-                                              spell2IdFromMatch:
-                                                  snapshot.data?.spell2Id,
-                                              spell2CoolDownFromMatch:
-                                                  snapshot.data?.spell2CoolDown,
-                                              championIdFromMatch:
-                                                  snapshot.data?.championId,
-                                              indexFromMatch: index,
-                                              colorFromMatch:
-                                                  Colors.blue.shade100),
-                                          ParticipantsList(
-                                              partsNamesFromMatch:
-                                                  snapshot.data?.partsNames,
-                                              spell1IdFromMatch:
-                                                  snapshot.data?.spell1Id,
-                                              spell1CoolDownFromMatch:
-                                                  snapshot.data?.spell1CoolDown,
-                                              spell2IdFromMatch:
-                                                  snapshot.data?.spell2Id,
-                                              spell2CoolDownFromMatch:
-                                                  snapshot.data?.spell2CoolDown,
-                                              championIdFromMatch:
-                                                  snapshot.data?.championId,
-                                              indexFromMatch: index + 5,
-                                              colorFromMatch:
-                                                  Colors.red.shade100),
-                                        ],
-                                      );
-                                    }
-                                    return Padding(
-                                      padding: const EdgeInsets.all(5.0),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Container(
-                                            width: 350,
-                                            child: ParticipantsList(
-                                                partsNamesFromMatch:
-                                                    snapshot.data!.partsNames,
-                                                spell1IdFromMatch:
-                                                    snapshot.data!.spell1Id,
-                                                spell1CoolDownFromMatch:
-                                                    snapshot
-                                                        .data!.spell1CoolDown,
-                                                spell2IdFromMatch:
-                                                    snapshot.data!.spell2Id,
-                                                spell2CoolDownFromMatch:
-                                                    snapshot
-                                                        .data!.spell2CoolDown,
-                                                championIdFromMatch:
-                                                    snapshot.data!.championId,
-                                                indexFromMatch: index + 5,
-                                                colorFromMatch:
-                                                    Colors.red.shade100),
-                                          ),
-                                        ],
-                                      ),
-                                    );
-                                  }),
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Stack(
+                          alignment: Alignment.topLeft,
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.arrow_back),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
                             ),
-                          ])
-                        ],
-                      );
-                    } else {
-                      return AlertDialog(
-                        title: Text("Error"),
-                        content: Text(
-                          mapTypeError,
-                          maxLines: 2,
-                          softWrap: true,
+                          ],
                         ),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Get.off(() => SearchSummoner());
-                            },
-                            child: Text("OK"),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Profile(
+                                summonerNameFromMatch: gotSummonerName,
+                                profileIconIdFromMatch: gotProfileIconId,
+                                summonerLevelFromMatch: gotSummonerLevel),
+                            Column(
+                              children: <Widget>[
+                                Text(
+                                  "게임 모드: ${snapshot.data!.gameMode.toString()}",
+                                ),
+                                Text(
+                                  snapshot.data!.gameStartTime.toString(),
+                                ),
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    primary:
+                                        pressColor ? Colors.blue : Colors.red,
+                                  ),
+                                  child: Text(pressText ? "전체보기" : "상대만 보기"),
+                                  onPressed: () {
+                                    setState(() {
+                                      pressColor = !pressColor;
+                                      pressText = !pressText;
+                                    });
+                                  },
+                                ),
+                              ],
+                            ),
+                            GameLengthTimer(
+                                gameLengthFromMatch:
+                                    snapshot.data!.gameLength.toString()),
+                          ],
+                        ),
+                        Row(children: <Widget>[
+                          Flexible(
+                            child: ListView.separated(
+                                separatorBuilder: (context, index) => Divider(),
+                                shrinkWrap: true,
+                                scrollDirection: Axis.vertical,
+                                itemCount: 5,
+                                itemBuilder: (BuildContext context, int index) {
+                                  if (pressColor == false) {
+                                    return Row(
+                                      children: <Widget>[
+                                        ParticipantsList(
+                                            partsNamesFromMatch:
+                                                snapshot.data!.partsNames,
+                                            spell1IdFromMatch:
+                                                snapshot.data!.spell1Id,
+                                            spell1CoolDownFromMatch:
+                                                snapshot.data!.spell1CoolDown,
+                                            spell2IdFromMatch:
+                                                snapshot.data!.spell2Id,
+                                            spell2CoolDownFromMatch:
+                                                snapshot.data!.spell2CoolDown,
+                                            championIdFromMatch:
+                                                snapshot.data!.championId,
+                                            indexFromMatch: index,
+                                            colorFromMatch:
+                                                Colors.blue.shade100),
+                                        ParticipantsList(
+                                            partsNamesFromMatch:
+                                                snapshot.data!.partsNames,
+                                            spell1IdFromMatch:
+                                                snapshot.data!.spell1Id,
+                                            spell1CoolDownFromMatch:
+                                                snapshot.data!.spell1CoolDown,
+                                            spell2IdFromMatch:
+                                                snapshot.data!.spell2Id,
+                                            spell2CoolDownFromMatch:
+                                                snapshot.data!.spell2CoolDown,
+                                            championIdFromMatch:
+                                                snapshot.data!.championId,
+                                            indexFromMatch: index + 5,
+                                            colorFromMatch:
+                                                Colors.red.shade100),
+                                      ],
+                                    );
+                                  }
+                                  return Row(
+                                    children: <Widget>[
+                                      Container(
+                                        child: ParticipantsList(
+                                            partsNamesFromMatch:
+                                                snapshot.data!.partsNames,
+                                            spell1IdFromMatch:
+                                                snapshot.data!.spell1Id,
+                                            spell1CoolDownFromMatch:
+                                                snapshot.data!.spell1CoolDown,
+                                            spell2IdFromMatch:
+                                                snapshot.data!.spell2Id,
+                                            spell2CoolDownFromMatch:
+                                                snapshot.data!.spell2CoolDown,
+                                            championIdFromMatch:
+                                                snapshot.data!.championId,
+                                            indexFromMatch: index + 5,
+                                            colorFromMatch:
+                                                Colors.red.shade100),
+                                      ),
+                                    ],
+                                  );
+                                }),
                           ),
-                        ],
-                      );
-                    }
-                  } else {
+                        ])
+                      ],
+                    );
+                  } else if (snapshot.hasError) {
                     return AlertDialog(
-                      title: Text("Error"),
+                      title: Text("에러"),
                       content: Text(
                         notFound,
                         maxLines: 2,
@@ -206,32 +196,19 @@ class _MatchScreenState extends State<MatchScreen> {
                       actions: [
                         TextButton(
                           onPressed: () {
-                            Get.off(() => SearchSummoner());
+                            Navigator.of(context).pop();
                           },
                           child: Text("OK"),
                         ),
                       ],
                     );
                   }
+                  return CircularProgressIndicator();
               }
             },
           ),
         ),
       ),
     );
-  }
-
-  _matchType(int mapId, int queueType) {
-    if (queueType == 0) {
-      return Text("Custom game");
-    } else if (queueType == 420) {
-      return Text("Solo queue");
-    } else if (queueType == 430) {
-      return Text("Normal game");
-    } else if (queueType == 440) {
-      return Text("Flex queue");
-    } else {
-      return alert(context, matchTypeError, 200);
-    }
   }
 }
