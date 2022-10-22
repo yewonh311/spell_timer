d04116c516532e318659cfc690d25d0640cb8611
Yewon Hong <88092102+yewonhong@users.noreply.github.com>
2022-10-22 14:33:12 -0700

    Update profile.dart

--- lib/widgets/profile.dart
+++ lib/widgets/profile.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
   final String summonerNameFromMatch;
-  final String profileIconIdFromMatch;
-  final String summonerLevelFromMatch;
+  final int profileIconIdFromMatch;
+  final int summonerLevelFromMatch;
   Profile(
       {required this.summonerNameFromMatch,
       required this.profileIconIdFromMatch,
@@ -16,22 +16,12 @@ class Profile extends StatelessWidget {
         child: Padding(
             padding: const EdgeInsets.all(2.0),
             child: Row(children: <Widget>[
-              Stack(alignment: Alignment.center, children: <Widget>[
-                Container(
-                    height: 65,
-                    width: 65,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      image: DecorationImage(
-                          image: AssetImage(
-                            "assets/images/forms/profileForm.png",
-                          ),
-                          fit: BoxFit.fill),
-                    )),
+              Stack(children: <Widget>[
                 Container(
                     height: 60,
                     width: 60,
                     decoration: BoxDecoration(
+                      color: Colors.white,
                       shape: BoxShape.rectangle,
                       image: DecorationImage(
                           image: AssetImage(
@@ -39,34 +29,9 @@ class Profile extends StatelessWidget {
                           ),
                           fit: BoxFit.fill),
                     )),
-                Positioned.fill(
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                        height: 15,
-                        width: 30,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          image: DecorationImage(
-                              image: AssetImage(
-                                "assets/images/forms/levelForm.png",
-                              ),
-                              fit: BoxFit.fill),
-                        )),
-                  ),
-                ),
-                Positioned.fill(
-                    child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Text(
-                          summonerLevelFromMatch.toString(),
-                          style: TextStyle(color: Colors.white, fontSize: 12),
-                        ))),
+                Center(child: Text(summonerLevelFromMatch.toString()))
               ]),
-              Text(
-                summonerNameFromMatch,
-                overflow: TextOverflow.ellipsis,
-              ),
+              Text(summonerNameFromMatch),
             ])));
   }
 }
